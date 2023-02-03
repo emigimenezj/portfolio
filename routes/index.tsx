91c172b4db76f79bbf7eb863ae83f9cf0cbc2c32
Emilio Giménez <emigimenezj@zohomail.com>
2023-02-02 22:49:07 -0300

    Update home for support blog section

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,10 +1,14 @@
 export default function Home() {
   return (
     <main class="p-4">
-      <h1 class="text-2xl">¡Mi blog!</h1>
-      <a class="underline hover:text-blue-500" href="./blog/hello-world">
-        Entrar al post 1.
+      <h1 class="text-2xl">¡Esto es el home!</h1>
+      <a class="underline hover:text-blue-500" href="./blog">
+        BLOG
       </a>
+      <br/>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero sint eaque eligendi qui iusto quasi enim facilis consectetur magni id repellendus cum ab adipisci, voluptatem earum quae. Doloribus, adipisci possimus.
+      </p>
     </main>
   );
 }
