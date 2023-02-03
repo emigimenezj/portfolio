638ae0f0eddbf8d9d6adcd02b94f1e1678cf846e
Emilio Giménez <emigimenezj@zohomail.com>
2023-02-02 22:51:38 -0300

    Add blog section index with a posts' list

---
+++ routes/blog/index.tsx
@@ -0,0 +1,28 @@
+import { Handlers, PageProps } from '$fresh/server.ts';
+import { listPosts } from '../../utils/posts.ts';
+import { Post } from '../../types.d.ts';
+
+export const handler: Handlers = {
+    async GET(_, context) {
+        const posts = await listPosts();
+        return context.render({posts}); 
+    }
+}
+
+export default function Blog(props: PageProps) {
+    const { posts } = props?.data || {};
+
+    return (
+        <main class='p-4'>
+            <h1 class='text-4xl font-bold'>¡Estos son mis posts!</h1>
+            {posts.map((p: Post) => (
+                <article class='p-4'>
+                    <h2 class='text-2xl font-bold'>
+                        <a href={`./blog/${p.id}`} class='hover:text-blue-500'>{p.title}</a>
+                    </h2>
+                    <time>{Intl.DateTimeFormat('es').format(p.date)}</time>
+                </article>
+            ))}
+        </main>
+    );
+}
